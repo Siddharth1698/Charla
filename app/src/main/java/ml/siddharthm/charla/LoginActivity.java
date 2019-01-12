dbbb092b66ac6a62564458a8a40ebe34eb918c4d
Siddharth M <siddharth.m98@gmail.com>
2019-01-12 23:49:52 +0530

    Already have Account And Need new Account Link

--- app/src/main/java/ml/siddharthm/charla/LoginActivity.java
+++ app/src/main/java/ml/siddharthm/charla/LoginActivity.java
@@ -3,6 +3,11 @@ package ml.siddharthm.charla;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,6 +16,11 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
+    private Button LoginButton;
+    private ImageView PhoneLoginButton;
+    private EditText UserEmail,UserPassword;
+    private TextView NeedNewAccountLink,ForgotPassowrdLink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +29,29 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        InitializeFeilds();
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUserToRegisterActivity();
+            }
+        });
+    }
+
+
+
+    private void InitializeFeilds() {
+        LoginButton = (Button)findViewById(R.id.login_button);
+        PhoneLoginButton = (ImageView) findViewById(R.id.phone_login_button);
+        UserEmail = (EditText) findViewById(R.id.login_email);
+        UserPassword = (EditText)findViewById(R.id.login_password);
+        NeedNewAccountLink = (TextView)findViewById(R.id.need_new_account);
+        ForgotPassowrdLink = (TextView)findViewById(R.id.forget_password);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-
         if (currentUser != null){
             sendUserToMainActivity();
         }
@@ -34,6 +60,10 @@ public class LoginActivity extends AppCompatActivity {
     private void sendUserToMainActivity() {
         Intent sendIntent = new Intent(LoginActivity.this,MainActivity.class);
         startActivity(sendIntent);
+        }
 
+    private void sendUserToRegisterActivity() {
+        Intent regIntent = new Intent(LoginActivity.this,RegisterActivity.class);
+        startActivity(regIntent);
     }
 }
--- app/src/main/java/ml/siddharthm/charla/RegisterActivity.java
+++ app/src/main/java/ml/siddharthm/charla/RegisterActivity.java
@@ -1,13 +1,42 @@
 package ml.siddharthm.charla;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button createAccountButton;
+    private EditText UserEmail,UserPassword;
+    private TextView AlreadyHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        InitializeFeilds();
+        AlreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendUserToLoginActivity();
+            }
+        });
+    }
+
+    private void InitializeFeilds() {
+        createAccountButton = (Button)findViewById(R.id.register_button);
+        UserEmail = (EditText) findViewById(R.id.register_email);
+        UserPassword = (EditText)findViewById(R.id.register_password);
+        AlreadyHaveAccount = (TextView)findViewById(R.id.already_have_an_account);
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent regIntent = new Intent(RegisterActivity.this,LoginActivity.class);
+        startActivity(regIntent);
     }
 }
