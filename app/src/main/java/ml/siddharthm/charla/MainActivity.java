ba1242e5e5b35e7f99859e9702a6d5ca656f88e7
Siddharth M <siddharth.m98@gmail.com>
2019-01-13 18:58:16 +0530

    Adding Logout Feature

--- app/src/main/java/ml/siddharthm/charla/MainActivity.java
+++ app/src/main/java/ml/siddharthm/charla/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -54,5 +56,27 @@ public class MainActivity extends AppCompatActivity {
     private void sendUserToLoginAActivity() {
         Intent sendIntent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(sendIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.main_logout_option){
+            mAuth.signOut();
+            sendUserToLoginAActivity();
+        }
+        if (item.getItemId() == R.id.main_logout_option){
+
+        }
+        if (item.getItemId() == R.id.main_logout_option){
+
+        }
+        return true;
     }
 }
